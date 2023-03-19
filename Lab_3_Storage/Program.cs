99b87ece1d7c3fa97939c1f121532a6fad898a55
Demid <demidberdin1234@gmail.com>
2023-03-19 22:37:02 +0500

    Lab_3 first commit

---
+++ Lab_3_Storage/Program.cs
@@ -0,0 +1,105 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        Storage<Point> q = new Storage<Point>();
+        LinkedList<Point> list = new LinkedList<Point>();
+        Random rnd = new Random();
+        int i = 100000;
+        for (i = 100000; i >= 0; i--)
+        {
+            int rand = rnd.Next(0, 40);
+            int j = rnd.Next(1, 3);
+            if (j == 1)
+            {
+                q.PushBack(new Point(rand, rand));
+                //list.AddLast(new Point(i, i));
+            }
+            else if (j == 2)
+            { 
+                q.PushFront(new Point(rand, rand));
+               // list.AddFirst(new Point(i, i));
+            }
+        }
+        i = 200000;
+        for (i = 200000; i >= 0; i--)
+        {
+            int j = rnd.Next(0, 40);
+            Point tmp = new Point(j, j);
+            int rand = rnd.Next(1, 4);
+            if (rand == 1)
+            {
+                if (q.Remove(tmp))
+                {
+                    //Console.WriteLine($"Deleting node with coord {tmp.X}, {tmp.Y}");
+                }   
+            }
+            if (rand == 2)
+            {
+                Node <Point>? nd = q.FindFirst(tmp);
+                //LinkedListNode<Point>? nd2 = list.Find(tmp);
+                if (nd != null)
+                {
+                    q.InsertAfter(tmp, nd);
+                    //Console.WriteLine($"Inserting node with coord {tmp.X}, {tmp.Y}");
+                }
+                //if (nd2 != null)
+                //{
+                //    list.AddAfter(nd2, tmp);
+                //}
+            }
+            if (rand == 3)
+            {
+                Node<Point>? nd = q.FindFirst(tmp);
+                if (nd != null)
+                {
+                    //nd.Value.Print();
+                }
+            }
+        }
+        Console.WriteLine($"\nList of All remaining nodes: {q.Count}\n");
+        //for (Node<Point>? ob = q.Head; q.Exists(ob); ob = ob.Next)
+       // {
+        //    ob.Value.Print();
+        //}
+        //Console.WriteLine("\n FOR LIST \n");
+        //for (LinkedListNode<Point>? ob = list.First; ob != null; ob = ob.Next)
+       // {
+        //    ob.Value.Print();
+       // }
+        stopWatch.Stop();
+        // Get the elapsed time as a TimeSpan value.
+        TimeSpan ts = stopWatch.Elapsed;
+
+        // Format and display the TimeSpan value.
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine($"RunTime {elapsedTime}");
+    }
+}
+class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Point(int x = 0, int y = 0)
+    {
+        X = x;
+        Y = y;
+    }
+    public override bool Equals(object? obj)
+    {
+        Point p = obj as Point;
+        return X == p.X && Y == p.Y;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"point with coords {X}, {Y}");
+    }
+}
---
+++ MyLib/Class1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//namespace MyNamespace
+//{
+    public class Node<T>
+    {
+        internal T value;
+        internal Node<T>? next;
+        internal Node<T>? prev;
+
+        public T Value
+        { 
+            get { return value; }
+            set { this.value = value; }
+        }
+        public Node<T>? Next
+        {
+            get { return next; }
+        }
+        public Node<T>? Prev
+        {
+            get { return prev; }
+        }
+        public Node(T value)
+        {
+            Value = value;
+            next = null;
+            prev = null;
+            //Console.WriteLine("Конструктор ноды");
+        }
+    }
+    public class Storage<K>
+    {
+        public Node<K>? Head { get; private set; }
+        public Node<K>? Tail { get; private set; }
+        public int Count { get; private set; }
+        public Storage()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+           // Console.WriteLine("Конструктор хранилища");
+        }
+        public void PushBack(K value)
+        {
+            if (Head == null)
+            {
+                Head = new Node<K>(value);
+                Tail = Head;
+                Count++;
+            }
+            else
+            {
+                Tail.next = new Node<K>(value);
+                Tail.Next.prev = Tail;
+                Tail = Tail.Next;
+                Count++;
+            }
+        }
+        public void PushFront(K value)
+        {
+            if (Head == null)
+            {
+                Head = new Node<K>(value);
+                Tail = Head;
+                Count++;
+            }
+            else
+            {
+                Head.prev = new Node<K>(value);
+                Head.prev.next = Head;
+                Head = Head.Prev;
+                Count++;
+            }
+        }
+        public bool Exists(Node<K>? node)
+        {
+            return node != null;
+        }
+        public void InsertAfter(K value, Node<K>? node)
+        {
+            Node<K> elem = new Node<K>(value);
+            if (Head == null)
+            {
+                Head = elem;
+                Tail = Head;
+                Count++;
+                return;
+            }
+            if (!Exists(node))
+            {
+                Console.WriteLine("Inserting After null, error");
+                return;
+            }
+            if(Exists(node.Next))
+            {
+                elem.next = node.Next;
+                node.next.prev = elem;
+            }
+            elem.prev = node;
+            node.next = elem;
+            Count++;
+        }
+        public Node<K>? FindFirst(K value)
+        {
+            Node<K>? elem = Head;
+            while (elem != null)
+            {
+                if(elem.Value.Equals(value))
+                {
+                    return elem;
+                }
+                elem = elem.next;
+            }
+            return null;
+        }
+        public Node<K>? FindLast(K value)
+        {
+            Node<K>? elem = Tail;
+            while (elem != null)
+            {
+                if (elem.Value.Equals(value))
+                {
+                    return elem;
+                }
+                elem = elem.prev;
+            }
+            return null;
+        }
+        public bool Remove(K value)
+        {
+            Node<K>? elem = FindFirst(value);
+            return Remove(elem);
+        }
+        public bool Remove(Node<K>? node)
+        {
+            if (node != null)
+            {
+                if (Count == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else if (node.prev == null)
+                {
+                    node.next.prev = node.prev;
+                    Head = node.next;
+                }
+                else if (node.next == null)
+                {
+                    node.prev.next = node.next;
+                    Tail = node.prev;
+                }
+                else
+                {
+                    node.next.prev = node.prev;
+                    node.prev.next = node.next;
+                }
+                Count--;
+                return true;
+            }
+            return false;
+        }
+    }
+//}
