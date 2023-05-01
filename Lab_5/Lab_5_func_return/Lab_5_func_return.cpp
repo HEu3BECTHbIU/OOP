9b89220c1fc7947a57b56624909bf9bdd9748037
Demid <demidberdin1234@gmail.com>
2023-05-02 01:44:54 +0500

    Добавлены функции проверки механизма возврата объектов

--- Lab_5/Lab_5_func_return/Lab_5_func_return.cpp
+++ Lab_5/Lab_5_func_return/Lab_5_func_return.cpp
@@ -13,7 +13,7 @@ public:
     {
         cout << "Констуктор Base от указателя" << endl;
     }
-    Base(Base& obj)
+    Base(const Base& obj)
     {
         cout << "Констуктор копирования Base" << endl;
     }
@@ -105,10 +105,84 @@ void Test2()
     delete obj1;
 }
 
+Base Func1()
+{
+    cout << "\nFunc1:" << endl;
+    Base obj;
+    return obj;
+}
+
+Base *Func2()
+{
+    cout << "\nFunc2:" << endl;
+    Base obj;
+    return &obj;
+}
+
+Base &Func3()
+{
+    cout << "\nFunc3:" << endl;
+    Base obj;
+    return obj;
+}
+Base Func4()
+{
+    cout << "\nFunc4:" << endl;
+    Base* obj = new Base(); // утечка памяти, т.к *obj никто не удаляет
+    return *obj;
+}
+
+Base *Func5()
+{
+    cout << "\nFunc5:" << endl;
+    Base* obj = new Base();
+    return obj;
+}
+Base &Func6()
+{
+    cout << "\nFunc6:" << endl;
+    Base* obj = new Base();
+    return *obj;
+}
+void Test3()
+{
+    {
+        Base base = Func1();
+        base.foo();
+    }
+    {
+        Base* base2 = Func2();
+        base2->foo(); // UB - обращение к методу объекта, которого уже не существует
+        // delete base2 // крашнет программу, т.к объект уже удален
+    }
+    {
+        Base &base3 = Func3();
+        base3.foo(); // UB - обращение к методу объекта, которого не существует
+    }
+    {
+        Base base4 = Func4();
+        base4.foo();
+    }
+    {
+        Base *base5 = Func5();
+        base5->foo();
+        delete base5;
+    }
+    {
+        Base &base6 = Func6();
+        base6.foo();
+        delete& base6;
+    }
+}
+
+void Test4()
+{
+
+}
 int main()
 {
     setlocale(LC_ALL, "rus");
     //Test1();
-    Test2();
-    //Test3();
+    //Test2();
+    Test3();
 }
